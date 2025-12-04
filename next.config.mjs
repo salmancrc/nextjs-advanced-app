9b8c7a6dbd185d37920005ea4b53381f30b6a21e
salman <salmancrc9@gmail.com>
2025-12-04 23:29:30 +0500

    chore: add experimental configuration for turbopack file system cache in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true
+  }
 };
 
 export default nextConfig;
